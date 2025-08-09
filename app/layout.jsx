c83af4726303601d5be3e162ff80ca7621c71707
unknown <ayshathulmushrifamushrifa@gmail.com>
2025-08-09 13:12:36 +0530

    Updated code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,22 @@
 // app/layout.js
+import './globals.css'
+
 export const metadata = {
   title: 'Colour of Art',
-  description: 'Your site description',
-  other: {
-    'google-adsense-account': 'ca-pub-7496144172676724',
-  },
+  description: 'Your site description here',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+        {children}
     </html>
   )
 }
