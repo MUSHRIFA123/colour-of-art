96667a656841a0a436beff101432b272f1374f25
unknown <ayshathulmushrifamushrifa@gmail.com>
2025-08-09 13:04:32 +0530

    Updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,13 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
       <body>{children}</body>
     </html>
   )
