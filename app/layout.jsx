c2854b070d63279d79b27d39ae49db87932079ba
unknown <ayshathulmushrifamushrifa@gmail.com>
2025-08-09 12:54:38 +0530

    Updated code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,20 @@
-// app/layout.jsx
-import "./globals.css";
-
+// app/layout.js
 export const metadata = {
-  title: "Your Site Title",
-  description:
-    "Discover unique handmade paintings and digital artwork. Support independent artists. Free shipping available.",
-};
+  title: 'Your Site Title',
+  description: 'Your site description',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
         {children}
     </html>
-  );
+  )
 }
